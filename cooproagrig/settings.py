7df771b492cdd4cb30cc13de71512c5d44c2d78c
dylanQ12 <136197614+dylanQ12@users.noreply.github.com>
2024-08-01 22:24:43 -0500

    updated code of files

--- cooproagrig/settings.py
+++ cooproagrig/settings.py
@@ -12,11 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-zwi0k)dz=yg0v*wk!4c)ph%rqoyus17xfj!$_x*$590vr0mj+z"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
-
-# ALLOWED_HOSTS = ['62.72.63.158', 'srv566477.hstgr.cloud']
+ALLOWED_HOSTS = ["cooproagri.org"]
 
 # Application definition
 INSTALLED_APPS = [
