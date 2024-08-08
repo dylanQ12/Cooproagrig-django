06ba83f1665937cce283c1b858588231767f259c
dylanQ12 <136197614+dylanQ12@users.noreply.github.com>
2024-08-08 16:39:50 -0500

    updated files and code

--- login/views.py
+++ login/views.py
@@ -2,22 +2,22 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
-# Create your views here.
+
 def loginView(request):
     context = {
-        'page_title': 'Iniciar sesión - Cooproagrig | Cooperativa de Producción Agrícola'
+        "page_title": "Iniciar sesión - Cooproagrig | Cooperativa de Producción Agrícola"
     }
-    
-    if request.method == 'POST':
-        username = request.POST.get('usuario')
-        password = request.POST.get('password')
+
+    if request.method == "POST":
+        username = request.POST.get("usuario")
+        password = request.POST.get("password")
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('dashboard')
+            return redirect("dashboard")
         else:
-            messages.error(request, 'Nombre de usuario o clave incorrectos.')
-    return render(request, 'login.html', context)
+            messages.error(request, "Nombre de usuario o clave incorrectos.")
+    return render(request, "login.html", context)
 
 
 # Vista del Logout para cerrar sesión.
